dbe4a14b623d0c9144205611cf04576e53c43661
shivakar <shivakar@ch.iitr.ac.in>
2021-11-08 00:13:26 +0530

    [shivakar] add: UC3_Queue is added

--- Program.cs
+++ Program.cs
@@ -6,13 +6,21 @@ namespace Stack_Queue_problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            LinkedListStack stack = new LinkedListStack();
+
+            /*LinkedListStack stack = new LinkedListStack();
             stack.push(70);
             stack.push(30);
             stack.push(56);
             stack.display();
-            stack.isempty();
+            stack.isempty();*/
+
+            LinkedListQueue Queue = new LinkedListQueue();
+            Queue.Enqueu(70);
+            Queue.Enqueu(30);
+            Queue.Enqueu(56);
+            Queue.display();
+      
+
         }
     }
 }
